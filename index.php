9885a03395183a1659f37a33dd9bb4a665cb5164
Erick <erick_3041210064@utd.edu.mx>
2022-11-29 11:33:31 -0600

    v2

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <h1>Soy index</h1>
     <h1>Soy index</h1>
     <h1>Soy index modificado desde GitHub remoto</h1>
+    <h1>Soy el index modificado desde VS code en local</h1>
 </body>
 </html>
